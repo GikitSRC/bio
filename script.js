fae39c7db739dc1588bf1b7876a6fb264868419b
ChainedTears <123793568+GikitSRC@users.noreply.github.com>
2023-07-24 18:46:21 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function daysUntilSeptember23() {
+      const today = new Date();
+      const targetDate = new Date(today.getFullYear(), 8, 23); 
+      if (today > targetDate) {
+        targetDate.setFullYear(targetDate.getFullYear() + 1);
+      }
+      const timeDiff = targetDate.getTime() - today.getTime();
+      const days = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+      return days;
+    }
+    document.getElementById('countdown').textContent = daysUntilSeptember23();
