20626e2f8332dfcdd7950fd63da4aa9f835cbd07
ChainedTears <123793568+GikitSRC@users.noreply.github.com>
2023-09-28 21:14:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function daysUntilSeptember23() {
-      const today = new Date();
-      const targetDate = new Date(today.getFullYear(), 8, 23); 
-      if (today > targetDate) {
-        targetDate.setFullYear(targetDate.getFullYear() + 1);
-      }
-      const timeDiff = targetDate.getTime() - today.getTime();
-      const days = Math.ceil(timeDiff / (1000 * 3600 * 24));
+  const today = new Date();
+  const targetDate = new Date(today.getFullYear(), 8, 23);
 
-      return days;
-    }
-    document.getElementById('countdown').textContent = daysUntilSeptember23();
+  if (today > targetDate) {
+    targetDate.setFullYear(targetDate.getFullYear() + 1);
+  }
+
+  const timeDiff = targetDate.getTime() - today.getTime();
+  const days = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+  if (days === 0) {
+    return "TODAY";
+  } else {
+    return days + " days";
+  }
+}
+
+document.getElementById('countdown').textContent = daysUntilSeptember23();
