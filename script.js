e6e375970eb3b5de1194a1ffe99939c4eaaafa11
ChainedTears <123793568+GikitSRC@users.noreply.github.com>
2023-10-02 22:01:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const currentDate = new Date();
-  const targetDate = new Date(currentDate.getFullYear(), 8, 23);
-  const timeDiff = targetDate - currentDate;
-  const daysUntilSept23 = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
-  const countdownElement = document.getElementById("countdown");
-  if (currentDate.getMonth() === 8 && currentDate.getDate() === 23) {
-    countdownElement.textContent = "today";
-  } else {
-    countdownElement.textContent = daysUntilSept23;
-  }
+document.addEventListener("DOMContentLoaded", function() {
+    const currentDate = new Date();
+    const targetDate = new Date(currentDate.getFullYear(), 8, 23);
+    const timeDiff = targetDate - currentDate;
+    const daysUntilMyBirthday = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+    const countdownElement = document.getElementById("countdown");
+    if (currentDate.getMonth() === 8 && currentDate.getDate() === 23) {
+      countdownElement.textContent = "today";
+    } else {
+      countdownElement.textContent = daysUntilMyBirthday;
+    }
+  });
