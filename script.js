b28916f81e2b48c4956be626b1578667b67123f0
ChainedTears <123793568+GikitSRC@users.noreply.github.com>
2023-10-02 22:01:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const daysUntilMyBirthday = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
     const countdownElement = document.getElementById("countdown");
     if (currentDate.getMonth() === 8 && currentDate.getDate() === 23) {
-      countdownElement.textContent = "today";
+      countdownElement.textContent = "TODAY IS MY BIRTHDAY!!!";
     } else {
       countdownElement.textContent = daysUntilMyBirthday;
     }
